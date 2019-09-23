32a918862d0963f2c9410cd401516c1f579b458e
Mohammad Harris <harris@tmrnd.com.my>
2019-09-23 10:17:31 +0800

    Use the latest OIP store for Centurion - Service API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.oip.tm.com.my/app/t/tmrnd.com.my/mycent/1.0.0/',
+  baseUrl: 'https://api.oip.tm.com.my/app/t/tmrnd.com.my/centurion/services/1/',
   uploadUrl: 'https://ticketing-centurion.apps.cp.tmrnd.com.my/',
   oipToken: 'Bearer 56975d5e-5d82-3f5f-8ed8-438ca080a22e'
 };
