1d28894ace061bf4d4d3bae1cb65f294bc12d3cb
unknown <ros@tmrnd.com.my>
2019-09-24 17:20:06 +0800

    update checkout page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  validatingForm: FormGroup;
+  cek = true;
+  marked = false;
+  count = 0;
+  type = 'company';
+  totalItem;
+  totalPrice;
+  cartCount: number;
+  totalToken: number;
+
+  
+  
+  btnCheckout = "";
+
 
   constructor() { }
 
   ngOnInit() {
+    this.validatingForm = new FormGroup({
+     projectname: new FormControl(null, Validators.required),
+     projectdesc: new FormControl(null, Validators.required)
+    });
   }
 
+  get projectname() {
+    // console.log(this.validatingForm.get('projectname').status)
+    // if (this.validatingForm.get('projectname').status === 'VALID') {
+    //   this.marked = true;
+    // }
+    
+    return this.validatingForm.get('projectname');
+   }
+
+  get projectdesc() {
+    // if (this.validatingForm.get('projectdesc').status === 'VALID') {
+    //   this.marked = true;
+    // }
+    return this.validatingForm.get('projectdesc');
+   }
+
+   Accept(e:any) {
+    // this.count++;
+    // if (this.count % 2) {
+    //   this.marked = true;
+    // } else {
+    //   this.marked = false;
+    // // }
+    const projectname = this.validatingForm.get('projectname').value;
+    const projectdesc = this.validatingForm.get('projectdesc').value;
+    console.log("aaaa"+projectname);
+    console.log("bbbb"+projectdesc);
+    if (false) {
+          this.cek = false;
+        }
+  }
+
+
+
 }
+
