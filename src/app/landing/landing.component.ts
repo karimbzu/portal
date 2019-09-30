50bdba454df2071c564c6e48241e1ed857bacd66
Mohammad Harris <harris@tmrnd.com.my>
2019-09-30 09:22:32 +0800

    Use landing.html as temporary landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { NavbarComponent } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, AfterViewInit {
+//  @ViewChild('navbar', { static: true }) navbar: NavbarComponent
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+//    this.navbar.toggle();
   }
 
 }
