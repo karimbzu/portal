48b5a8fa5eee6606f4a8ab8f752bcc7d0e7d26b5
Mohammad Harris <harris@tmrnd.com.my>
2019-10-30 15:41:14 +0800

    ready for new release development

--- src/app/header-new/header-new.component.ts
+++ src/app/header-new/header-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   selector: 'app-header-new',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-new.component.scss']
 })
 export class HeaderNewComponent implements OnInit {
+  cartCount: number;
 
-  constructor() { }
+  constructor(private myCart: CartService) { }
 
   ngOnInit() {
+    this.myCart.currentCartValue.subscribe(val => this.cartCount = val);
   }
 
 }
--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TicketService} from '../../services/ticket.service';
 
 @Component({
   selector: 'app-ticket',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent implements OnInit {
+  ticketInfo;
   ticketList = [];
-  constructor() {
+  constructor(private myTicket: TicketService) {
+    this.myTicket.currentTicketInfo.subscribe(val => {
+      this.ticketInfo = val;
 
-    }
-   
+      // Perform some processing
+      val.forEach(item => {
+        if (item.ticketId !== 0) {
+          this.ticketList.push ( '#' + item.ticketId);
+        }
+      });
+    });
+  }
+
 
   ngOnInit() {
   }
