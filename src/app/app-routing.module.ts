9002b8c381d389ac1f8daa3b71f4846fa455f7f8
Rosmawati Ab Raub <ros@tmrnd.com.my>
2019-10-31 17:08:47 +0800

    edit UI
    Signed-off-by: Rosmawati Ab Raub <ros@tmrnd.com.my>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { ScanRequestComponent } from './scan-request/scan-request.component';
 import { TicketComponent } from './ticket/ticket.component';
-import { DisabledManualURLGuard } from '../services/can-activate-route.guard';
+// import { DisabledManualURLGuard } from '../services/can-activate-route.guard';
 import { BlockedComponent } from './blocked/blocked.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import {Page1Component} from './page1/page1.component';
@@ -17,13 +17,15 @@ const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'landing', component: LandingComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'my-cart', component: MyCartComponent, canActivate: [DisabledManualURLGuard]},
+  {path: 'my-cart', component: MyCartComponent},
+  // {path: 'my-cart', component: MyCartComponent, canActivate: [DisabledManualURLGuard]},
   {path: 'my-account', component: MyAccountComponent},
   {path: 'scan-request', component: ScanRequestComponent},
   {path: 'ticket', component: TicketComponent},
   {path: 'scan-request', component: ScanRequestComponent},
   {path: 'blocked', component: BlockedComponent},
-  {path: 'checkout', component: CheckoutComponent, canActivate: [DisabledManualURLGuard]},
+  {path: 'checkout', component: CheckoutComponent},
+  // {path: 'checkout', component: CheckoutComponent, canActivate: [DisabledManualURLGuard]},
   {path: 'page1', component: Page1Component},
   {path: 'dashboard', component: DashboardComponent}
 ];
