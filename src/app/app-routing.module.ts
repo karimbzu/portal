5bdaef1d02ed4aa8cc1d7e7d0be340a4ef0942e1
azliza <liza@tmrnd.com.my>
2019-11-07 16:59:34 +0800

    1. create component for order history 2.create sub menu for order history 3. create table pagingation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MyCartComponent } from './my-cart/my-cart.component';
 import { MyAccountComponent } from './my-account/my-account.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { ScanRequestComponent } from './scan-request/scan-request.component';
@@ -28,6 +29,7 @@ const routes: Routes = [
   {path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard, DisabledManualURLGuard]},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'widget', component: WidgetComponent, canActivate: [AuthGuard]},
+  {path: 'order-history', component: OrderHistoryComponent, canActivate: [AuthGuard]},
   {path: '**', component: ErrorComponent}
 ];
 
