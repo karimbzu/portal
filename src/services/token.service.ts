c2328a823bd08b8dff0f95c08a6eced2a458cb7d
Rosmawati Ab Raub <ros@tmrnd.com.my>
2019-11-25 17:18:06 +0800

    create token/credit service
    Signed-off-by: Rosmawati Ab Raub <ros@tmrnd.com.my>

---
+++ src/services/token.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  export class TokenService {
+    private tokenAmount = new BehaviorSubject(0);
+    currentTokenAmount = this.tokenAmount.asObservable();
+  
+   
+    constructor(public http: HttpClient) {
+     
+     }
+
+     getTokenBalance() {
+        if (!localStorage.getItem('authToken')) {
+          console.error ('getUserToken', 'No authToken available for this user');
+          return;
+        }
+    
+        // Fetch the data
+        this.http.get(environment.baseUrl + 'ticketing/cart', {
+          headers: new HttpHeaders()
+            // .set('Authorization', environment.oipToken)
+            .set('x-auth-token', localStorage.getItem('authToken')),
+          observe: 'response'
+        }).subscribe((response: any) => {
+          console.log (response.body.info);
+          if (response.body.info !== undefined) {
+            // Update the number of carts in list
+            // this.tokenAmount.next (response.body.info.length);
+    
+            // Update the list
+            this.tokenAmount.next (response.body.info);
+          }
+        });
+      }
+}  
+
