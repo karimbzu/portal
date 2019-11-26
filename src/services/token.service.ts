8da3f971168dbe113e357d07130982be390133a6
Rosmawati Ab Raub <ros@tmrnd.com.my>
2019-11-26 17:06:37 +0800

    add deduct()
    Signed-off-by: Rosmawati Ab Raub <ros@tmrnd.com.my>

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -41,5 +41,30 @@ import { environment } from '../environments/environment';
         });
       }
 
+      deductToken(charging) {
+        if (!localStorage.getItem('authToken')) {
+          console.error ('deductToken', 'No authToken available for this user');
+          return;
+        }
+    
+        // Fetch the data
+        this.http.get(environment.baseUrl + 'ticketing/token/deduct/' + charging, {
+          
+          headers: new HttpHeaders()
+            .set('Authorization', environment.oipToken)
+            .set('x-auth-token', localStorage.getItem('authToken')),
+          observe: 'response'
+        }).subscribe((response: any) => {
+          console.log (response.body.info);
+          if (response.body.info !== undefined) {
+            // Update the number of carts in list
+            // this.tokenAmount.next (response.body.info.length);
+    
+            // Update the list
+            this.tokenAmount.next (response.body.info.amount);
+          }
+        });
+      }
+
 }  
 
