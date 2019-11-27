0f5347e6acf0aa153c28c06e611fde52d532bdab
Rosmawati Ab Raub <ros@tmrnd.com.my>
2019-11-27 10:36:14 +0800

    add topup()
    Signed-off-by: Rosmawati Ab Raub <ros@tmrnd.com.my>

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -66,5 +66,30 @@ import { environment } from '../environments/environment';
         });
       }
 
+      topupToken(topup) {
+        if (!localStorage.getItem('authToken')) {
+          console.error ('topupToken', 'No authToken available for this user');
+          return;
+        }
+    
+        // Fetch the data
+        this.http.get(environment.baseUrl + 'ticketing/token/topup/' + topup, {
+          
+          headers: new HttpHeaders()
+            .set('Authorization', environment.oipToken)
+            .set('x-auth-token', localStorage.getItem('authToken')),
+          observe: 'response'
+        }).subscribe((response: any) => {
+          console.log (response.body.info);
+          if (response.body.info !== undefined) {
+            // Update the number of carts in list
+            // this.tokenAmount.next (response.body.info.length);
+    
+            // Update the list
+            this.tokenAmount.next (response.body.info.amount);
+          }
+        });
+      }
+
 }  
 
