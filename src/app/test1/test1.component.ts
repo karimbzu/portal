ea7a9f89ccac9c4e4aa028699ee00b958fc923a3
Mohammad Harris <harris@tmrnd.com.my>
2020-01-10 11:26:17 +0800

    test the toast opacity

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {ToastService} from 'ng-uikit-pro-standard';
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.scss']
+})
+export class Test1Component implements OnInit {
+  toastOption = {
+    opacity: 1
+  };
+
+  constructor(private myToast: ToastService) { }
+
+  ngOnInit() {
+  }
+
+  sendMessage1() {
+    this.myToast.success('This is Message 1', 'Success', this.toastOption);
+  }
+
+  sendMessage2() {
+    this.myToast.info('This is Message 2', 'Info');
+  }
+
+}
